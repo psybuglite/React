aedbe6d8bafa8dfd4ea93585e3dadeb883aff5e0
tonye-d <itztonye@gmail.com>
2018-01-16 21:27:02 +0100

    finished day 9

--- 30days/src/Footer.js
+++ 30days/src/Footer.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 
 
-
-
-
 class Footer extends React.Component {
-  render() {
-    return (
-      <div className='footer'>
-        {this.props.children}
-      </div>
-    )
-  }
+    render() {
+        return ( <
+            div className = 'footer' > { this.props.children } <
+            /div>
+        )
+    }
 }
-
-
 
 export default Footer;
